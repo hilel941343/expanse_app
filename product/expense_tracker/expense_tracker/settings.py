864363114a71596455c3fb2ff362fa22b2042a4d
hilel <hilel941343@gmail.comm>
2025-05-06 15:28:26 +0300

    "Add Procfile for Render deployment"

--- product/expense_tracker/expense_tracker/settings.py
+++ product/expense_tracker/expense_tracker/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -134,3 +134,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Redirect to home page after login/logout
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+
+DEBUG = os.getenv('RENDER', '') != 'true'
